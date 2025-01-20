fc9813cf57033c01493ce3e4c04e0bcdc694f962
nadera <nanajuhana@nadras.local>
2025-01-20 11:05:41 +0700

    create pop-up notification

--- lib/ui/home_screen.dart
+++ lib/ui/home_screen.dart
@@ -1,10 +1,108 @@
+import 'package:attadence_app/ui/attend/attendance_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return PopScope( //untuk maksure mau di back atau ngga
+      canPop: false,
+      // ignore: deprecated_member_use
+      onPopInvoked: (didPop) {
+        if (didPop) {
+          return;
+        }
+        _onWillPop;
+      },
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+           children: [
+
+          Container(
+            margin: const EdgeInsets.all(10),
+            child: Expanded(
+              child: InkWell(
+                highlightColor: Colors.transparent ,
+                splashColor: Colors.transparent,
+                onTap: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => const AttendanceScreen()));
+                },
+            child: const Column(
+              children: [
+                Image(
+                  image: AssetImage('/assets/images/ic_attend.png'),
+                  width: 100,
+                  height: 100 , 
+                ),
+                Text(
+                  "Attendance",
+                  style: TextStyle(
+                    fontSize: 14,
+                    fontWeight: FontWeight.bold
+                  ),
+                )
+              ],
+            ),
+              )),
+          ),
+          const SizedBox(height: 40,)
+           ],
+          ),
+        ),
+      ),
+    );
   }
-}
+
+Future<bool> _onWillPop(BuildContext context) async {
+  return (await showDialog(
+    barrierDismissible: false,
+    context: context, 
+    builder: (context) => 
+    AlertDialog(
+      title: const Text("Information",
+      style: TextStyle(
+        color: Colors.black,
+        fontSize: 20,
+        fontWeight: FontWeight.bold,
+        ),
+      ),
+      content: const Text(
+        "Do yoy want to Texi?",
+        style: TextStyle(
+          color: Colors.black,
+          fontSize: 16
+        ),
+      ),
+      actions: [
+        TextButton(
+          //() => Navigator.of(context).pop(false)
+          onPressed:() => Navigator.of(context).pop(false), 
+          child: const Text(
+            "No",
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 14
+            ),
+          )
+          ),
+
+          TextButton(onPressed: () => SystemNavigator.pop(), 
+          child: const Text(
+            " Yes I want to Exit",
+            style: TextStyle(
+              color: Colors.white
+            ),
+          )
+        )
+      ],
+      
+            )
+          )
+        );
+      }
+    }
