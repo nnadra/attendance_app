992bb8a7feb59e2cffa42498685cc0890edb5ef2
nadera <nanajuhana@nadras.local>
2025-02-05 10:47:57 +0700

    mmake a static snackbar

---
+++ lib/ui/permission/components/snack_bar.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class SnackBarComponent {
+  //statis model => mempertahankan value dari func tanpa pangil objek
+  static void showSnackBar(BuildContext context, String message, {bool isError = false}) {
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+      content: Row(
+        children: [
+          Icon(
+            isError ? Icons.error_outline : Icons.check_circle_outline,
+            color: Colors.white,
+          ),
+          const SizedBox(width: 10),
+          Expanded(
+            child: Text(
+              message,
+              style: const TextStyle(
+                color: Colors.white
+              ),
+            )
+          )
+        ],
+      ),
+      backgroundColor: isError? Colors.red : Colors.grey ,
+      shape: StadiumBorder(),
+      behavior: SnackBarBehavior.floating,
+      )
+      );
+
+  }
+
+}
